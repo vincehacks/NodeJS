4a7e3b507b5fba646418a5ea5384ed1d4a724f1c
Vince Chang <vincechang@Vinces-MacBook-Pro.local>
2018-05-03 22:59:59 -0700

    Spacing fixes

--- NodeSchool/learnyounode/babysteps.js
+++ NodeSchool/learnyounode/babysteps.js
@@ -7,44 +7,43 @@
   and prints the sum of those numbers to the console (stdout).
  *****************************************************************************
   HINTS:
-	
+
   You can access command-line arguments via the global process object. The
   process object has an argv property which is an array containing the
   complete command-line. i.e. process.argv.
 
-	To get started, write a program that simply contains:
+  To get started, write a program that simply contains:
 
   console.log(process.argv)
 
-	Run it with node program.js and some numbers as arguments. e.g:
+  Run it with node program.js and some numbers as arguments. e.g:
 
-	$ node program.js 1 2 3    // This should equal 1 + 2 + 3 = 6
+  $ node program.js 1 2 3    // This should equal 1 + 2 + 3 = 6
 
-	In which case the output would be an array looking something like:
+  In which case the output would be an array looking something like:
 
-	[ 'node', '/path/to/your/program.js', '1', '2', '3' ]
-	 
-	You'll need to think about how to loop through the number arguments so
-	you can output just their sum. The first element of the process.argv array
-	is always 'node', and the second element is always the path to your
-	program.js file, so you need to start at the 3rd element (index 2), adding
-	each item to the total until you reach the end of the array.
+  [ 'node', '/path/to/your/program.js', '1', '2', '3' ]
 
-	Also be aware that all elements of process.argv are strings and you may
-	need to coerce them into numbers. You can do this by prefixing the
-	property with + or passing it to Number(). e.g. +process.argv[2] or
-	Number(process.argv[2]).
+  You'll need to think about how to loop through the number arguments so
+  you can output just their sum. The first element of the process.argv array
+  is always 'node', and the second element is always the path to your
+  program.js file, so you need to start at the 3rd element (index 2), adding
+  each item to the total until you reach the end of the array.
 
+  Also be aware that all elements of process.argv are strings and you may
+  need to coerce them into numbers. You can do this by prefixing the
+  property with + or passing it to Number(). e.g. +process.argv[2] or
+  Number(process.argv[2]).
  ****************************************************************************/
-  
-	// Save the process length
-	var processSize = process.argv.length;
-	var sum = 0;
 
-	// Loop through and add the numbers together, need to cast string to number.
-	for(i = 2;  i < processSize; i++){
-		sum += Number(process.argv[i]);
-	}
+// Save the process length
+var processSize = process.argv.length;
+var sum = 0;
 
-	// Print the output
-	console.log(sum);
+// Loop through and add the numbers together, need to cast string to number.
+for(i = 2;  i < processSize; i++){
+  sum += Number(process.argv[i]);
+}
+
+// Print the output
+console.log(sum);
