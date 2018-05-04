c91f5d2c0d24fa8c86d3251a34fe1b8e5cc0f4a3
Vince Chang <vincechang@Vinces-MacBook-Pro.local>
2018-05-03 21:16:46 -0700

    made changes

--- NodeSchool/learnyounode/babysteps.js
+++ NodeSchool/learnyounode/babysteps.js
@@ -9,12 +9,12 @@
   HINTS:
 	
   You can access command-line arguments via the global process object. The
-	process object has an argv property which is an array containing the
-	complete command-line. i.e. process.argv.
+  process object has an argv property which is an array containing the
+  complete command-line. i.e. process.argv.
 
 	To get started, write a program that simply contains:
 
-	console.log(process.argv)
+  console.log(process.argv)
 
 	Run it with node program.js and some numbers as arguments. e.g:
 
