5ca0e790397a6e75b1b8d26213b34abc717051ff
Vince Chang <vincechang@Vinces-MacBook-Pro.local>
2019-03-10 23:43:17 -0700

    JS practice added

---
+++ JS_expiramentation/test.js
@@ -0,0 +1,11 @@
+var myInt = 5;
+var myFloat = 6.667;
+myInt;
+myFloat;
+console.log(typeof myInt);
+console.log(typeof myFloat);
+
+
+var myDate = '19' + '67';
+console.log(typeof myDate);
+console.log(myDate);
